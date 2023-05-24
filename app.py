7626df10cbee8fb7f4f75ac5472772945a1696ce
bh6539 <123916373+bh6539@users.noreply.github.com>
2023-05-24 13:10:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "WEB TEST 124"
+    return "WEB TEST 1234"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
